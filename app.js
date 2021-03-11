462b9641d0c393c4706adeba2761294c4fdcc64d
Christine Lyston <calyston.14@gmail.com>
2021-03-11 17:02:03 +0000

    feat: mute buttons functional

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ class DrumKit {
     this.playButton = document.querySelector('.play');
     this.isPlaying = null;
     this.selectSound = document.querySelectorAll('select');
+    this.muteButton = document.querySelectorAll('.mute');
   }
   activePad() {
     this.classList.toggle('active');
@@ -77,6 +78,35 @@ class DrumKit {
         break;
     }
   }
+  mute(e) {
+    const muteIndex = e.target.getAttribute("data-track");
+    e.target.classList.toggle('active');
+    if (e.target.classList.contains('active')) {
+      switch (muteIndex) {
+        case "0":
+          this.kickAudio.volume = 0;
+          break;
+        case "1":
+          this.snareAudio.volume = 0;
+          break;
+        case "2":
+          this.hihatAudio.volume = 0;
+          break;
+      }
+    } else {
+      switch (muteIndex) {
+        case "0":
+          this.kickAudio.volume = 1;
+          break;
+        case "1":
+          this.snareAudio.volume = 1;
+          break;
+        case "2":
+          this.hihatAudio.volume = 1;
+          break;
+      }
+    }
+  }
 }
 
 const drumKit = new DrumKit();
@@ -101,5 +131,13 @@ drumKit.playButton.addEventListener('click', function () {
 drumKit.selectSound.forEach(select => {
   select.addEventListener('change', function (e) {
     drumKit.changeSound(e);
+  })
+})
+
+//Mute Button
+
+drumKit.muteButton.forEach(button => {
+  button.addEventListener('click', function (e) {
+    drumKit.mute(e);
   })
 })
